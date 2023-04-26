ab7701fb33ca8f5ac1727ad63f054515b92054aa
Bluerax <50401077+Bluerax@users.noreply.github.com>
2023-04-26 23:19:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,52 @@
 # -*- coding: utf-8 -*-
 
 import os
+import asyncio
 import requests
+import pyppeteer
 import json
 from bs4 import BeautifulSoup
+
+async def main():
+    # 启动无头浏览器
+    browser = await pyppeteer.launch(headless=True)
+    page = await browser.newPage()
+
+    # 52签到页
+    await page.goto("https://www.52pojie.cn/home.php?mod=task&do=apply&id=2")
+
+    # 设置cookies
+    cookies_list = os.environ['52PjCookie']
+    for cookie in cookies_list:
+        # 添加cookies
+        await page.setCookie(cookie)
+
+    # 刷新页面
+    await page.reload()
+    await asyncio.sleep(2)
+    content = await page.content()
+ 
+    # 获取签到结果并推送
+    result, fc = check(content)
+    print(result + fc)
+    token = os.environ['PushToken']
+    pushplus_push(token= token, title=result, content=fc, topic='')
+
+    # 关闭浏览器
+    await browser.close()
+
+def check(content):
+    fb = BeautifulSoup(content, "html.parser")
+    fc = fb.find("div", id="messagetext").find("p").text
+    if "您需要先登录才能继续本操作" in fc:
+        result = "52Cookie失效"
+    elif "恭喜" in fc:
+        result = "52签到成功"
+    elif "已申请过此任务" in fc:
+        result = "52今日已签到"
+    else:
+        result = "52签到失败"
+    return result, fc
 
 def pushplus_push(token, title, content, topic):
     url = 'http://www.pushplus.plus/send'
@@ -15,53 +58,12 @@ def pushplus_push(token, title, content, topic):
         'topic': topic
     }
     response = requests.post(url, headers=headers, data=json.dumps(data))
+    print(response.json())
     return response.json()
 
 
-
-
-
-def sign(cookie):
-    result = ""
-    headers = {
-        "Cookie": cookie,
-        "ContentType": "text/html;charset=gbk",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Language': 'zh-CN,zh;q=0.9',
-        'Referer': 'https://www.52pojie.cn/',
-        'Connection': 'keep-alive',
-        'Upgrade-Insecure-Requests': '1'
-    }
-    requests.session().put(
-        "https://www.52pojie.cn/home.php?mod=task&do=apply&id=2referer=%2F", headers=headers
-    )
-    fa = requests.session().put(
-        "https://www.52pojie.cn/home.php?mod=task&do=draw&id=2referer=%2F", headers=headers
-    )
-    fb = BeautifulSoup(fa.text, "html.parser")
-    fc = fb.find("div", id="messagetext").find("p").text
-    if "您需要先登录才能继续本操作" in fc:
-        result += "Cookie 失效"
-    elif "恭喜" in fc:
-        result += "签到成功"
-    elif "不是进行中的任务" in fc:
-        result += "今日已签到"
-    else:
-        result += "签到失败"
-    
-    pushplus_push(token=token, title=result, content=result, topic='')
-    return result 
-
-def main():
-    b = os.environ['PojieCookie']
-    cookie = b
-    token = os.environ['PushToken']
-    sign_msg = sign(cookie=cookie)
-    print(sign_msg)
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    # 启动事件循环
+    asyncio.get_event_loop().run_until_complete(main())
 
 
